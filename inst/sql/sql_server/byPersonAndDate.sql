9bb9f63bf6eca77b4b76e9a339f74872f32b267b
jdposada <jdposa@gmail.com>
2022-08-10 12:47:32 -0500

    Create byPersonAndDate.sql

---
+++ inst/sql/sql_server/byPersonAndDate.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 
+ @resultDatabaseSchema.@cdmTable AS
+  SELECT
+    table_.*
+  FROM
+    @cdmDatabaseSchema.@cdmTable AS table_
+  JOIN
+    @resultDatabaseSchema.@cohortTable AS cohort
+  ON
+    table_.person_id = cohort.subject_id
+  WHERE
+    cohort_definition_id = @cohortId
+    AND table_.@dateColumn BETWEEN cohort.cohort_start_date AND cohort.cohort_end_date
