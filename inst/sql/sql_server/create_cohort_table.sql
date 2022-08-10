db0b52cc022e56b6f248be44cea4072efacc8d64
jdposada <jdposa@gmail.com>
2022-08-10 12:33:49 -0500

    Create create_cohort_table.sql

---
+++ inst/sql/sql_server/create_cohort_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE @cdmDatabaseSchema.cohort (
+			cohort_definition_id integer NOT NULL,
+			subject_id integer NOT NULL,
+			cohort_start_date date NOT NULL,
+			cohort_end_date date NOT NULL );
